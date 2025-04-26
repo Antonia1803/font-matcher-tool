027a9e95e798a0d1820dfd922f9373ee860f5efe
Antonia1803 <lilablassblau1@gmx.de>
2025-04-26 13:08:07 +0200

    Deleted fontData.js content

--- fontData.js
+++ fontData.js
@@ -1,10 +1,1 @@
 
-export default [
-  { name: "Roboto", traits: ["freundlich", "einfach", "ehrlich"] },
-  { name: "Lora", traits: ["klassisch", "ehrlich", "natürlich"] },
-  { name: "Poppins", traits: ["modern", "innovativ", "freundlich"] },
-  { name: "Montserrat", traits: ["puristisch", "modern", "einfach"] },
-  { name: "Nunito", traits: ["freundlich", "familienfreundlich", "einfach"] },
-  { name: "Open Sans", traits: ["neutral", "lesbar", "ehrlich"] },
-  { name: "Merriweather", traits: ["natürlich", "lesbar", "klassisch"] }
-];
